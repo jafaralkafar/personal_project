ecb81e231169a8ad195b276fc2a71bef280fc920
Grant Fox <granttfox@gmail.com>
2019-06-25 09:28:48 -0600

    added routes and components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
+import Header from './components/Header'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" component={LandingPage} exact />
+        </Switch>
+      </Router>
     </div>
   );
 }
