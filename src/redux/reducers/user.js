afa419ebb0b0c1647f6c3319a04ab7f749121445
Grant Fox <granttfox@gmail.com>
2019-06-26 15:55:43 -0600

    added error handling to user reducer

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -3,22 +3,27 @@ import axios from 'axios'
 const GET_USER = 'GET_USER'
 const GET_USER_PENDING = 'GET_USER_PENDING'
 const GET_USER_FULFILLED = 'GET_USER_FULFILLED'
+const GET_USER_REJECTED = 'GET_USER_REJECTED'
 
 const REGISTER_USER = 'REGISTER_USER'
 const REGISTER_USER_PENDING = 'REGISTER_USER_PENDING'
 const REGISTER_USER_FULFILLED = 'REGISTER_USER_FULFILLED'
+const REGISTER_USER_REJECTED = 'REGISTER_USER_REJECTED'
 
 const LOGIN_USER = 'LOGIN_USER'
 const LOGIN_USER_PENDING = 'LOGIN_USER_PENDING'
 const LOGIN_USER_FULFILLED = 'LOGIN_USER_FULFILLED'
+const LOGIN_USER_REJECTED = 'LOGIN_USER_REJECTED'
 
 const LOGOUT_USER = 'LOGOUT_USER'
 const LOGOUT_USER_PENDING = 'LOGOUT_USER_PENDING'
 const LOGOUT_USER_FULFILLED = 'LOGOUT_USER_FULFILLED'
+const LOGOUT_USER_REJECTED = 'LOGOUT_USER_REJECTED'
 
 const initialState = {
     loading: false,
-    data: null
+    data: null,
+    error: null
 }
 
 export default function(state = initialState, action) {
@@ -27,18 +32,30 @@ export default function(state = initialState, action) {
             return { ...state, loading: true }
         case GET_USER_FULFILLED:
             return { ...state, data: action.payload.data, loading: false }
+        case GET_USER_REJECTED:
+            return { ...state, error: action.payload, loading: false }
+        
         case REGISTER_USER_PENDING:
             return { ...state, loading: true }
         case REGISTER_USER_FULFILLED:
             return { ...state, data: action.payload.data, loading: false }
+        case REGISTER_USER_REJECTED:
+            return { ...state, error: action.payload, loading: false }
+        
         case LOGIN_USER_PENDING:
             return { ...state, loading: true }
         case LOGIN_USER_FULFILLED:
             return { ...state, data: action.payload.data, loading: false }
+        case LOGIN_USER_REJECTED:
+            return { ...state, error: action.payload, loading: false }
+        
         case LOGOUT_USER_PENDING:
             return { ...state, loading: true }
         case LOGOUT_USER_FULFILLED:
             return { ...state, data: null, loading: false }
+        case LOGOUT_USER_REJECTED:
+            return { ...state, error: action.payload, loading: false}
+
         default:
             return state
     }
