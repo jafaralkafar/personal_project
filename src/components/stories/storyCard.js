d922b0cb2d28961040273abb8c89e4928ff6dc35
Grant Fox <granttfox@gmail.com>
2019-07-05 08:55:41 -0600

    added choice cards to story card

--- src/components/stories/storyCard.js
+++ src/components/stories/storyCard.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { FullStoryCard } from './StyledCard';
+import { Card } from '../StyledComponents/PreviewCards'
 
 const StoryCard = props => {
     return (
         <FullStoryCard>
             <div>
                 StoryCard
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Card>Choice</Card>
+                <Card>Choice</Card>
+                <Card>Choice</Card>
             </div>
         </FullStoryCard>
     )
