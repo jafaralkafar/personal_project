b7195ecf8da961825f231da7d16e3ca6d8857fc4
Grant Fox <granttfox@gmail.com>
2019-07-05 07:51:50 -0600

    added code inside choicecard component

---
+++ src/components/stories/choiceCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const ChoiceCard = props => {
+
+    return (
+        <div>
+            ChoiceCard
+        </div>
+    )
+}
+
+export default connect(null)(ChoiceCard)
